94e90e33b7690d9dd819e269c54bdf7bae66f0e4
TravisChen <travischenn@outlook.com>
2019-11-21 14:18:34 +0800

    feat

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -8,10 +8,10 @@ else
 fi
 npm i
 npm run build
-if [ -d "/usr/local/Cellar/nginx/1.17.3_1/html/lapt-local" ];then
-  rm -rf '/usr/local/Cellar/nginx/1.17.3_1/html/lapt-local'
+if [ -d "/usr/local/nginx/html/lapt-local" ];then
+  rm -rf '/usr/local/nginx/html/lapt-local'
 fi
-cp -aRf /Users/TravisChen/Desktop/ZhiHui-Package-Cli/jobs/Peppa-Admin/dist /usr/local/Cellar/nginx/1.17.3_1/html
+cp -aRf /root/travis/devops/ZhiHui-Package-Cli/jobs/Peppa-Admin/dist /usr/local/nginx/html
 cd /usr/local/Cellar/nginx/1.17.3_1/html || exit
 mv dist lapt-local
 nginx -s reload
