4e30f0f45db4cb65655c39a9742963a5887cc334
TravisChen <travischenn@outlook.com>
2019-11-21 14:14:24 +0800

    test

--- app/controller/webhook.js
+++ app/controller/webhook.js
@@ -1,21 +1,20 @@
 'use strict';
-// const { exec } = require('child_process');
+const { exec } = require('child_process');
 
 const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
   async webhook() {
     const { ctx } = this;
-    // ctx.logger.info('=====================');
-    // exec('./shell/deploy.sh', (error, stdout, stderr) => {
-    //   if (error) {
-    //     console.error(`执行的错误: ${error}`);
-    //     return;
-    //   }
-    //   console.log(`stdout: ${stdout}`);
-    //   console.error(`stderr: ${stderr}`);
-    // });
-    ctx.body = 'Hello World';
+    ctx.logger.info('=====================');
+    exec('./shell/deploy.sh', (error, stdout, stderr) => {
+      if (error) {
+        ctx.logger.error(`error: ${error}`);
+        return;
+      }
+      ctx.logger.info(`stdout: ${stdout}`);
+      ctx.logger.error(`stderr: ${stderr}`);
+    });
   }
 }
 
--- shell/deploy.sh
+++ shell/deploy.sh
@@ -1,4 +1,17 @@
-cp -aRf /Users/TravisChen/Desktop/ZhiHui-Package-Cli/lapt-beta/Peppa-Admin/dist /usr/local/Cellar/nginx/1.17.3_1/html
+cd jobs || exit
+if [ -d "Peppa-Admin" ];then
+  cd Peppa-Admin || exit
+  git pull
+else
+  git clone https://gitee.com/TravisChenn/Peppa-Admin.git
+  cd Peppa-Admin || exit
+fi
+npm i
+npm run build
+if [ -d "/usr/local/Cellar/nginx/1.17.3_1/html/lapt-local" ];then
+  rm -rf '/usr/local/Cellar/nginx/1.17.3_1/html/lapt-local'
+fi
+cp -aRf /Users/TravisChen/Desktop/ZhiHui-Package-Cli/jobs/Peppa-Admin/dist /usr/local/Cellar/nginx/1.17.3_1/html
 cd /usr/local/Cellar/nginx/1.17.3_1/html || exit
 mv dist lapt-local
 nginx -s reload
