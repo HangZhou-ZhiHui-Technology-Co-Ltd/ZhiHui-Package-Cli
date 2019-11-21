3c5b14dbfd4db312f994e3b185abdc775f6ac514
TravisChen <travischenn@outlook.com>
2019-11-21 15:51:52 +0800

    asd

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -1,8 +1,4 @@
-pwd
-if [ ! -d "jobs" ];then
-  mkdir jobs
-fi
-cd jobs || exit
+cd /root/travis/devops/jobs || exit
 if [ -d "Peppa-Admin" ];then
   cd Peppa-Admin || exit
   git pull
@@ -12,11 +8,11 @@ else
 fi
 npm i
 pwd
-#npm run build
-#if [ -d "/usr/local/nginx/html/lapt-beta" ];then
-#  rm -rf '/usr/local/nginx/html/lapt-beta'
-#fi
-#cp -aRf /root/travis/devops/ZhiHui-Package-Cli/jobs/Peppa-Admin/dist /usr/local/nginx/html
-#cd /usr/local/nginx/html || exit
-#mv dist lapt-beta
-#nginx -s reload
+npm run build
+if [ -d "/usr/local/nginx/html/lapt-beta" ];then
+  rm -rf '/usr/local/nginx/html/lapt-beta'
+fi
+cp -aRf /root/travis/devops/jobs/Peppa-Admin/dist /usr/local/nginx/html
+cd /usr/local/nginx/html || exit
+mv dist lapt-beta
+nginx -s reload
